9840410ae51f54a7317d7d494b0588affcdccf4f
unknown <ms3071687@gmail.com>
2026-02-13 19:55:33 +0500

    -----

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <Button>Test</Button>
+      </div>
+    </>
+  );
+}
